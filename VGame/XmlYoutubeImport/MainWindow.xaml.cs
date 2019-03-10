38c9dbbe7ab685fdbbb3830fa0c8995204c444ff
lvovandrey <lvovandrey@mail.ru>
2019-03-10 03:29:07 +0300

    Начал делать этот плеер. Всю голову себе изъебал с привязками.

--- VGame/XmlYoutubeImport/MainWindow.xaml.cs
+++ VGame/XmlYoutubeImport/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,109 @@ using System.Windows.Shapes;
 
 namespace XmlYoutubeImport
 {
+    public class SceneData : INotifyPropertyChanged
+    {   public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private TimeSpan begin;
+        public TimeSpan Begin
+        {
+            get { return begin; }
+            set
+            {
+                begin = value;
+                OnPropertyChanged("Begin");
+            }
+        }
+
+        private TimeSpan end;
+        public TimeSpan End
+        {
+            get { return end; }
+            set
+            {
+                end = value;
+                OnPropertyChanged("End");
+            }
+        }
+
+        int num;
+        public int Num
+        {
+            get { return num; }
+            set
+            {
+                num = value;
+                OnPropertyChanged("Num");
+            }
+        }
+
+    }
+
+
+    public  class VideoDataContext: INotifyPropertyChanged
+    {
+        public List<SceneData> sceneDatas;
+
+        public SceneData selectedSceneData;
+        public SceneData SelectedSceneData
+        {
+            get { return selectedSceneData; }
+            set
+            {
+                selectedSceneData = value;
+                OnPropertyChanged("SelectedSceneData");
+            }
+        }
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public static class Global
+    {
+        public static VideoDataContext VideoData;
+    }
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+    
         public MainWindow()
         {
+
             InitializeComponent();
+
+            Global.VideoData = new VideoDataContext();
+            DataContext = Global.VideoData;
+
+            Global.VideoData.Title = "Пустой заголовок";
+            Global.VideoData.SelectedSceneData = new SceneData();
+
+
         }
     }
 }
---
+++ VGame/XmlYoutubeImport/VideoPartsControl.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XmlYoutubeImport
+{
+
+    public class SceneVideo
+        {
+            public int Id { get; set; }
+            public TimeSpan TimeBegin { get; set; } // производитель
+            public TimeSpan TimeEnd { get; set; }
+            public string ImagePath { get; set; }// путь к изображению скриншота
+
+    }
+    /// <summary>
+    /// Логика взаимодействия для VideoPartsControl.xaml
+    /// </summary>
+    public partial class VideoPartsControl : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private SceneData test;
+        public SceneData TEST
+        {
+            get { return test; }
+            set
+            {
+                test = value;
+                OnPropertyChanged("test");
+            }
+        }
+
+
+        public VideoPartsControl()
+        {
+            InitializeComponent();
+            TEST = new SceneData { Begin = TimeSpan.FromSeconds(10) };
+
+            SceneVideos = new ObservableCollection<SceneVideo>
+        {
+            new SceneVideo() {ImagePath= @"C:\vlctmp.png", Id = 1, TimeEnd = TimeSpan.FromSeconds(123) },
+            new SceneVideo(),
+            new SceneVideo(),
+            new SceneVideo()
+        };
+            phonesList.ItemsSource = SceneVideos;
+        }
+
+        public ObservableCollection<SceneVideo> SceneVideos { get; set; }
+
+        private void phonesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SceneVideo p = (SceneVideo)phonesList.SelectedItem;
+
+           // Global.VideoData.selectedSceneData.Begin = p.TimeEnd;
+            TEST.Begin = p.TimeEnd;
+        }
+    }
+}
