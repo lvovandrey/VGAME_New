969532d7b0775f9192d76d596bf893e2d55a846e
lvovandrey <lvovandrey@mail.ru>
2021-01-24 18:08:20 +0300

    Готово - Кнопка Настройки - криво сделан курсор (плохо наводится) #36

---
+++ VGame/VanyaGame/Interface/BeautyButtonLevelAbort.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VanyaGame.Interface
+{
+    /// <summary>
+    /// Interaction logic for BeautyButtonLevelAbort.xaml
+    /// </summary>
+    public partial class BeautyButtonLevelAbort : UserControl
+    {
+        public BeautyButtonLevelAbort()
+        {
+            InitializeComponent();
+        }
+
+        bool isFlieing = false;
+
+        private void Img_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var uri = new Uri("pack://application:,,,/Images/HandPushDown.cur");
+            var stream = Application.GetResourceStream(uri).Stream;
+            var cursor = new Cursor(stream);
+            Img.Cursor = cursor;
+        }
+
+        private void Img_MouseLeave(object sender, MouseEventArgs e)
+        {
+            var uri = new Uri("pack://application:,,,/Images/HandPush.cur");
+            var stream = Application.GetResourceStream(uri).Stream;
+            var cursor = new Cursor(stream);
+            Img.Cursor = cursor;
+
+        }
+
+        private void Img_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            isFlieing = true;
+
+            var uri = new Uri("pack://application:,,,/Images/HandPush.cur");
+            var stream = Application.GetResourceStream(uri).Stream;
+            var cursor = new Cursor(stream);
+            Img.Cursor = cursor;
+
+        }
+
+
+        private void Img_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (!isFlieing)
+            {
+                var uri = new Uri("pack://application:,,,/Images/HandPush.cur");
+                var stream = Application.GetResourceStream(uri).Stream;
+                var cursor = new Cursor(stream);
+                Img.Cursor = cursor;
+            }
+        }
+
+    }
+}
