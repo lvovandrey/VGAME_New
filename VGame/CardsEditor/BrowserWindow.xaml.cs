f824b86fd00e68bbcc5a8cb04ecbad1824b28a7e
lvovandrey <lvovandrey@mail.ru>
2021-02-17 20:10:25 +0300

    Ну двавай попробуем откатить

---
+++ VGame/CardsEditor/BrowserWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CardsEditor
+{
+    /// <summary>
+    /// Interaction logic for BrowserWindow.xaml
+    /// </summary>
+    public partial class BrowserWindow : Window
+    {
+        public BrowserWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
---
+++ VGame/InternetBrowser/Browser.xaml.cs
@@ -0,0 +1,66 @@
+﻿using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InternetBrowser
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class Browser : UserControl
+    {
+        public WebBrowserVM WebBrowserVM;
+
+        public event Action<string> ChoiceUrl;
+
+        public Browser()
+        {
+            CefSettings settings = new CefSettings();
+            settings.CachePath = @"C:\CEFcookies";
+            CefSharp.Cef.Initialize(settings);
+
+            InitializeComponent();
+
+
+
+            WebBrowserVM = new WebBrowserVM(MyBrowser);
+            LifespanHandler life = new LifespanHandler();
+            MyBrowser.LifeSpanHandler = life;
+            life.popup_request += life_popup_request;
+            MyBrowser.DataContext = WebBrowserVM;
+        }
+
+        private void life_popup_request(string obj) 
+        {
+            Dispatcher.Invoke(() => { WebBrowserVM.CurURL = obj; });
+             
+        }
+
+        private void ButtonGo_Click(object sender, RoutedEventArgs e)
+        {
+            WebBrowserVM.CurURL = TextURL.Text;
+        }
+
+        private void TextURL_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter) WebBrowserVM.CurURL = TextURL.Text;
+        }
+
+        private void ButtonChoiceUrl_Click(object sender, RoutedEventArgs e)
+        {
+            ChoiceUrl(WebBrowserVM.CurURL);
+        }
+    }
+}
